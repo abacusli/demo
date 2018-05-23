7ebb354b79b181dc7c780d14f5e32dbe2f8e995d
abacusli <179084436@qq.com>
2018-05-23 17:38:36 +0800

    m

---
+++ JavaProjectDemo/trunk/Code/src/test/java/demo/StringUtils.java
@@ -0,0 +1,16 @@
+package demo;
+
+public abstract class StringUtils {
+
+    /**
+     * 
+     * @param s
+     * @return
+     * @author abacus.li
+     * @date 2018年5月23日
+     *
+     */
+    public static String trim(String s) {
+        return null == s ? null : s.trim();
+    }
+}
